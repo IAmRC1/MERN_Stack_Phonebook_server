6a8d443981e398531e1043f445c084e1243d1a84
Rishabh Singh <rishabhcena1@gmail.com>
2020-04-19 21:18:41 +0530

    adding the build folder

---
+++ build/precache-manifest.e2252cbed2c4f7026ae398480c947d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690ff52f82c569da8e7c62cdcc027108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ba6de4e77bd001575f",
+    "url": "/static/css/2.8185e9ba.chunk.css"
+  },
+  {
+    "revision": "e6abf81fecfd5f5d9502",
+    "url": "/static/css/main.200f8ccb.chunk.css"
+  },
+  {
+    "revision": "a3ba6de4e77bd001575f",
+    "url": "/static/js/2.d61c4eca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d61c4eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6abf81fecfd5f5d9502",
+    "url": "/static/js/main.20c09db3.chunk.js"
+  },
+  {
+    "revision": "35ac0cb231f09e2d7f8e",
+    "url": "/static/js/runtime-main.6b4cc74e.js"
+  }
+]);
